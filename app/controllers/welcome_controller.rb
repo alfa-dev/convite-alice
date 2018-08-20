f052101af3e2a37099943d2ad44671589463496e
Rafael Lopes <rafael.mediario@gmail.com>
2018-08-19 21:02:12 -0300

    One more text review

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,13 @@ class WelcomeController < ApplicationController
 
   def index
     @name = params[:n]
+
+    if(@name.include? ' e ')
+      @prefix = ", "
+    else
+      @prefix = " e "
+    end
+
     @days_remaining = (Date.parse("2018-09-04") - Date.today).to_i
   end
 end
