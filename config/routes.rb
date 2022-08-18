247624bc5396f124dfe7c5ce5c2c6bf835f101b1
Rafael Alfa <rafael.mediario@gmail.com>
2022-08-18 14:19:32 -0300

    Remove ald version

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'welcome#index'
-
-  get '/g/:n', to:'welcome#generator'
-  get '/:n', to:'welcome#index'
-end
